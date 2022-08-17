24ecd32fd86636fffb17e5a94d3a3ef82c11311e
ex-dream-online <111167101+ex-dream-online@users.noreply.github.com>
2022-08-17 15:18:52 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -7,11 +7,8 @@ import time
 import sys
 import openpyxl
 import os
-import pandas as pd
 import numpy
-import numpy as np
-import pandas as pd
-import pandas
+
 
 global bv,header
 
@@ -22,9 +19,9 @@ header={
 }
 
 # 验证bv号是否正确
-def get_code():
+def get_code_judge():
     urla='http://api.bilibili.com/x/web-interface/archive/stat?bvid='
-    js=requests.get(urla+bv,headers=header).text
+    js=requests.session().get(urla+bv,headers=header).text
     raw=re.search(r'"code":.*?,',str(js)).group(0)
     code=re.search(r'\d+',str(raw)).group(0)
     if int(code)!=0:
@@ -34,7 +31,7 @@ def get_code():
 def get_aid():
     global aid
     urla='http://api.bilibili.com/x/web-interface/archive/stat?bvid='
-    js=requests.get(urla+bv,headers=header).text
+    js=requests.session().get(urla+bv,headers=header).text
     # 数据正则筛选
     raw=re.search(r'"aid":\d+',str(js)).group(0)
     aid=re.search(r'\d+',str(raw)).group(0)
@@ -44,7 +41,7 @@ def get_cid():
     global cid
     urla='https://api.bilibili.com/x/player/pagelist?bvid='
     urlb='&jsonp=jsonp'
-    js=requests.get(urla+bv+urlb,headers=header).text
+    js=requests.session().get(urla+bv+urlb,headers=header).text
     # 数据正则筛选
     raw=re.search(r'"cid":\d+',str(js)).group(0)
     cid=re.search(r'\d+',str(raw)).group(0)
@@ -53,83 +50,48 @@ def get_cid():
 def get_title():
     global title
     urla='https://api.bilibili.com/x/web-interface/view?bvid='
-    js=requests.get(urla+bv,headers=header).text
+    js=requests.session().get(urla+bv,headers=header).text
     js=eval("u"+"\'"+js+"\'")# 处理编码问题
     # 数据正则
     raw=re.search(r'"title":".*?"',str(js)).group(0)
     raw1=re.split(r'"',raw)
     title=raw1[3]
     return title
-# 获取播放量
-def get_view():
-    global view
+# 获取视频数据 播放量，弹幕数，回复数，点赞数，硬币数，分享数，点赞数
+def get_data():
+    global view,danmaku,reply,favorite,coin,share,like
     urla='http://api.bilibili.com/x/web-interface/archive/stat?bvid='
-    js=requests.get(urla+bv,headers=header).text
-    # 数据正则筛选
-    raw=re.search(r'"view":\d+',str(js)).group(0)
-    view=re.search(r'\d+',str(raw)).group(0)
+    js=requests.session().get(urla+bv,headers=header).text
+    # 数据筛选
+    # 播放量
+    raw_view=re.search(r'"view":\d+',str(js)).group(0)
+    view=re.search(r'\d+',str(raw_view)).group(0)
+    # 弹幕数
+    raw_danmaku=re.search(r'"danmaku":\d+',str(js)).group(0)
+    danmaku=re.search(r'\d+',str(raw_danmaku)).group(0)
+    # 回复数
+    raw_reply=re.search(r'"reply":\d+',str(js)).group(0)
+    reply=re.search(r'\d+',str(raw_reply)).group(0)
+    # 收藏数
+    raw_favorite=re.search(r'"favorite":\d+',str(js)).group(0)
+    favorite=re.search(r'\d+',str(raw_favorite)).group(0)
+    # 硬币数
+    raw_coin=re.search(r'"coin":\d+',str(js)).group(0)
+    coin=re.search(r'\d+',str(raw_coin)).group(0)
+    # 分享数
+    raw_share=re.search(r'"share":\d+',str(js)).group(0)
+    share=re.search(r'\d+',str(raw_share)).group(0)
+    # 点赞数
+    raw_like=re.search(r'"like":\d+',str(js)).group(0)
+    like=re.search(r'\d+',str(raw_like)).group(0)
     return view
-# 获取弹幕数
-def get_danmaku():
-    global danmaku
-    urla='http://api.bilibili.com/x/web-interface/archive/stat?bvid='
-    js=requests.get(urla+bv,headers=header).text
-    # 数据正则筛选
-    raw=re.search(r'"danmaku":\d+',str(js)).group(0)
-    danmaku=re.search(r'\d+',str(raw)).group(0)
-    return danmaku
-# 获取评论数
-def get_reply():
-    global reply
-    urla='http://api.bilibili.com/x/web-interface/archive/stat?bvid='
-    js=requests.get(urla+bv,headers=header).text
-    # 数据正则筛选
-    raw=re.search(r'"reply":\d+',str(js)).group(0)
-    reply=re.search(r'\d+',str(raw)).group(0)
-    return reply
-# 获取收藏数
-def get_favorite():
-    global favorite
-    urla='http://api.bilibili.com/x/web-interface/archive/stat?bvid='
-    js=requests.get(urla+bv,headers=header).text
-    # 数据正则筛选
-    raw=re.search(r'"favorite":\d+',str(js)).group(0)
-    favorite=re.search(r'\d+',str(raw)).group(0)
-    return favorite
-# 获取硬币数
-def get_coin():
-    global coin
-    urla='http://api.bilibili.com/x/web-interface/archive/stat?bvid='
-    js=requests.get(urla+bv,headers=header).text
-    # 数据正则筛选
-    raw=re.search(r'"coin":\d+',str(js)).group(0)
-    coin=re.search(r'\d+',str(raw)).group(0)
-    return coin
-# 获取硬币数
-def get_share():
-    global share
-    urla='http://api.bilibili.com/x/web-interface/archive/stat?bvid='
-    js=requests.get(urla+bv,headers=header).text
-    # 数据正则筛选
-    raw=re.search(r'"share":\d+',str(js)).group(0)
-    share=re.search(r'\d+',str(raw)).group(0)
-    return share
-# 获取点赞数
-def get_like():
-    global like
-    urla='http://api.bilibili.com/x/web-interface/archive/stat?bvid='
-    js=requests.get(urla+bv,headers=header).text
-    # 数据正则筛选
-    raw=re.search(r'"like":\d+',str(js)).group(0)
-    like=re.search(r'\d+',str(raw)).group(0)
-    return like
 # 获取当前观看人数
 def get_concurrent_viewers():
     global concurrent_viewers
     urla='https://api.bilibili.com/x/player/online/total?aid='
     urlb='&cid='
     urlc='&bvid='
-    js=requests.get(urla+aid+urlb+cid+urlc+bv,headers=header).text
+    js=requests.session().get(urla+aid+urlb+cid+urlc+bv,headers=header).text
     # 数据正则筛选
     raw=re.search(r'"total":"\d+',str(js)).group(0)
     concurrent_viewers=re.search(r'\d+',str(raw)).group(0)
@@ -171,15 +133,24 @@ def write_excel_xlsx_append(path,value,truncate_sheet=False):
         sheet.cell(row=startrows+1,column=i).value=value[i-1]
     workbook.save(path)
 
+
 bv=input('请输入您想要爬取的视频的BV号:')
-get_code()
+get_code_judge()
 n=int(input('请输入您想爬取的数据条数(每两条数据间隔大概30s,您可以在下方更改间隔时间):'))
 get_aid();get_cid();get_title()
 for i in range(n):
     #延时
     if i>0:
         time.sleep(40+random.randint(-7,15))
-    get_view();get_reply();get_share();get_like();get_concurrent_viewers();get_concurrent_time();
+    try:
+        get_data()
+        get_concurrent_viewers()
+    except Exception:
+        print('Zzzzzz...')
+        time.sleep(240)
+        get_data()
+        get_concurrent_viewers()
+    get_concurrent_time()
     print('播放量:%d'%int(view),'评论数:%d'%int(reply),'分享数:%d'%int(share),'点赞数:%d'%int(like),
           '当前观看人数:%s'%concurrent_viewers,'数据记录时间:%s'%time1,'剩余抓取次数:%d'%int(n-i-1))
     xx_info=[view,reply,share,like,concurrent_viewers,time1]
